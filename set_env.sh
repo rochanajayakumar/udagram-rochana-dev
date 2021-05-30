4a2abc7681d2fa3239a755d81c92f26754fa0fe3
rochana <rochana.jayakumar@gmail.com>
2021-05-30 18:16:53 -0400

    Make everything root directory

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=rochana
+export POSTGRES_PASSWORD=Wolfpack1309
+export POSTGRES_HOST=database-1.cuabuxlwbhhj.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-632497565445-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
